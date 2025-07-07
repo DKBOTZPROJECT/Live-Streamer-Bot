28a7106dd345dfb5a71263e5f0177c250a2769ea
DKBOTZ <108566855+DKBOTZPROJECT@users.noreply.github.com>
2025-07-07 10:24:02 +0530

    Cmd Messages

---
+++ text.py
@@ -0,0 +1,12 @@
+START_MESSAGE = """🎥 <b>Welcome To The YouTube Live Streamer Bot!</b>
+
+✨ I Am Here To Help You Stream <b>24/7 Videos</b> On:
+🔴 <b>YouTube
+🔵 Facebook
+🟣 And Many Other Platforms!</b>
+
+💡 Whether You Are A Creator, Brand, Or Business, I Will Make Your <b>Live Streaming</b> Easy And Non-Stop For You.
+
+ℹ️ <b>Want To Know More?</b> 
+👉 Tap The Buttons Below!
+"""
