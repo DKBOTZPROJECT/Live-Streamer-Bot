e9fd2df3e69c4867379aba0087ad4f715a5f90ef
DKBOTZ <108566855+DKBOTZPROJECT@users.noreply.github.com>
2025-07-07 10:16:24 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,1 +1,7 @@
+import os, re
+from os import environ
 
+
+API_ID = int(environ.get("API_ID", "1234567"))
+API_HASH = environ.get("API_HASH", "your_api_hash")
+BOT_TOKEN = environ.get("BOT_TOKEN", "your_bot_token")
