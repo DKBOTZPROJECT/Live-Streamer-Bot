cfcb53a5eee2e6e24c61efa889f6d59b95f7dce9
DKBOTZ <108566855+DKBOTZPROJECT@users.noreply.github.com>
2025-07-07 10:28:07 +0530

    SUPPORT ADDED

--- Config.py
+++ Config.py
@@ -5,3 +5,9 @@ from os import environ
 API_ID = int(environ.get("API_ID", "1234567"))
 API_HASH = environ.get("API_HASH", "your_api_hash")
 BOT_TOKEN = environ.get("BOT_TOKEN", "your_bot_token")
+
+
+
+# Info 
+CHANNEL_USERNAME = environ.get("CHANNEL_USERNAME", "DKBOTZ")
+DEVELOPER_USERNAME = environ.get("DEVELOPER_USERNAME", "DKBOTZPRO")
