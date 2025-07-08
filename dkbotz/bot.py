701b30ac870ce3cdf706cabadbef9fab66aae2d3
DKBOTZ <108566855+DKBOTZPROJECT@users.noreply.github.com>
2025-07-08 17:54:01 +0530

    Help Cmd

--- dkbotz/bot.py
+++ dkbotz/bot.py
@@ -5,11 +5,20 @@ from text import *
 
 
 
-
+# /start cmd
 @DKBOTZ.on_message(filters.command("start"))
-async def handle_start(bot, message):
+async def dkbotz_handle_start(bot, message):
     await message.reply_text(
         START_MESSAGE,
         reply_markup=start_buttons(),
+        disable_web_page_preview=True
+    )
+
+# /help cmd
+@DKBOTZ.on_message(filters.command("help"))
+async def dkbotz_help_command(bot, message):
+    await message.reply_text(
+        HELP_MESSAGE,
+        reply_markup=dkbotz_help_buttons(),
         disable_web_page_preview=True
     )
