0110f8b53fb608815d3aad24a16205c7fb9b038a
DKBOTZ <108566855+DKBOTZPROJECT@users.noreply.github.com>
2025-07-08 19:08:31 +0530

    Payments Method

--- Config.py
+++ Config.py
@@ -11,3 +11,11 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "your_bot_token")
 # Info 
 CHANNEL_USERNAME = environ.get("CHANNEL_USERNAME", "DKBOTZ")
 DEVELOPER_USERNAME = environ.get("DEVELOPER_USERNAME", "DKBOTZPRO")
+
+# Payment Method
+QR_IMAGE_URL = environ.get("QR_IMAGE_URL", "https://premium.dkbotzpro.in/QR.jpg")
+UPI_ID = environ.get("UPI_ID", "dkbotz@ybl")
+CRYPTO = environ.get("CRYPTO", "Contact To @DKBOTZHELP")
+PAYTM = environ.get("PAYTM", "dkbotz@ybl")
+PHONEPE = environ.get("PHONEPE", "dkbotz@ybl")
+PAYPAL = environ.get("PAYPAL", "Contact To @DKBOTZHELP")
