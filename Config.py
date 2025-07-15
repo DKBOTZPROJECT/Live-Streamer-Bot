0d02e4e9f613ad6cd9f116c73bc674cee7c5518b
DKBOTZ <108566855+DKBOTZPROJECT@users.noreply.github.com>
2025-07-15 08:48:36 +0530

    Video Uploader Config

--- Config.py
+++ Config.py
@@ -22,6 +22,11 @@ PAYPAL = environ.get("PAYPAL", "Contact To @DKBOTZHELP")
 
 
 # premium.dkkbotzpro.in Account Details
-PREMIUM_USERNAME = environ.get("PREMIUM_USERNAME")
-PREMIUM_PASSWORD = environ.get("PREMIUM_PASSWORD")
+PREMIUM_USERNAME = environ.get("PREMIUM_USERNAME", "")
+PREMIUM_PASSWORD = environ.get("PREMIUM_PASSWORD", "")
 PRODUCT_NAME = "Traffic-Tool-V1"
+
+
+# Video Uploaser
+BYTESCALE_ACCOUNT_ID = environ.get("BYTESCALE_ACCOUNT_ID", "")
+BYTESCALE_PUBLIC_KEY = environ.get("BYTESCALE_PUBLIC_KEY", "")
