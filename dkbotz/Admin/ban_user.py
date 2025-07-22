f76b0f0da1f015fa8bdf6f995ee093d7927fbf6d
DKBOTZ <108566855+DKBOTZPROJECT@users.noreply.github.com>
2025-07-22 09:07:48 +0530

    Ban User

---
+++ dkbotz/Admin/ban_user.py
@@ -0,0 +1,47 @@
+import logging
+import traceback
+from Config import *
+from helpers import *
+
+log = logging.getLogger(__name__)
+
+
+@DKBOTZ.on_message(filters.private & filters.command("ban_user") & filters.user(AUTH_USERS))
+async def dkbotz_ban_admin(c, m):
+
+    if len(m.command) == 1:
+        await m.reply_text(
+            "Use this command to ban any user from the bot.\n\nUsage:\n\n`/ban_user user_id "
+            "ban_duration ban_reason`\n\nEg: `/ban_user 1234567 28 You misused me.`\n This will "
+            "ban user with id `1234567` for `28` days for the reason `You misused me`.",
+            quote=True,
+        )
+        return
+
+    try:
+        user_id = int(m.command[1])
+        ban_duration = int(m.command[2])
+        ban_reason = " ".join(m.command[3:])
+        ban_log_text = f"Banning user {user_id} for {ban_duration} day(s) for the reason {ban_reason}."
+
+        try:
+            await c.send_message(
+                user_id,
+                f"You are banned to use this bot for **{ban_duration}** days for "
+                f"the reason __{ban_reason}__ \n\n**Message from the admin**",
+            )
+            ban_log_text += "\n\nUser notified successfully ✅"
+        except Exception as e:
+            log.debug(e, exc_info=True)
+            ban_log_text += (
+                f"\n\nUser notification failed !!!\n\n`{traceback.format_exc()}`"
+            )
+        await db.ban_user(user_id, ban_duration, ban_reason)
+        log.debug(ban_log_text)
+        await m.reply_text(ban_log_text, quote=True)
+    except Exception as e:
+        log.error(e, exc_info=True)
+        await m.reply_text(
+            f"Error occoured! Traceback given below\n\n`{traceback.format_exc()}`",
+            quote=True,
+        )
