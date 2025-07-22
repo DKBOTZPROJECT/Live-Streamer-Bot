01fea5fdc9c8d788187fa465072a9edb86cbb4f4
DKBOTZ <108566855+DKBOTZPROJECT@users.noreply.github.com>
2025-07-22 09:05:15 +0530

    Create status.py

---
+++ dkbotz/Admin/status.py
@@ -0,0 +1,9 @@
+from Config import *
+from helpers import *
+
+
+@DKBOTZ.on_message(filters.private & filters.command("status") & filters.user(AUTH_USERS))
+async def dkbotz_status_admin(c, m):
+    total_users = await db.total_users_count()
+    text = f"Total users till date : {total_users}"
+    await m.reply_text(text=text, quote=True)
